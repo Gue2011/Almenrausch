7bfbd56fa0519077644a721a3942da3eac298f78
Christian Günther <gue119@web.de>
2011-11-04 08:35:29 +0100

    Spent some comments

--- src/de/iplabs/almenrausch/LoginServlet.java
+++ src/de/iplabs/almenrausch/LoginServlet.java
@@ -11,20 +11,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import de.iplabs.almenrausch.web.Router;
 
+/**
+ * Servlet that handles login stuff.
+ * 
+ * @author gue
+ */
 @SuppressWarnings("serial")
-public class LoginServlet extends HttpServlet {
-	
+public class LoginServlet extends HttpServlet 
+{
+	// A logger for this class. 
 	Logger log = Logger.getLogger(LoginServlet.class.getName());
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		
+	@SuppressWarnings("unused")
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	{
 		final String login = req.getParameter("pw"); 
 		
+		log.info("Enter some password detenction in produc tion environments!"); 
+		// "wenzel007".equals(login)
 		if (1 == 1)
 		{
 			req.getSession().setAttribute("login", true); 
-			GregorianCalendar gc = new GregorianCalendar(); 
+			final GregorianCalendar gc = new GregorianCalendar(); 
 			gc.setTime(new Date()); 
 			Router.redirect(resp, "/viewTasks?week="+gc.get(GregorianCalendar.WEEK_OF_YEAR - 1)); 
 		}
